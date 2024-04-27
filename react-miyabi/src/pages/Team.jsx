47f4481363c666d27540235a410ccd7611b9c15c
Alyssa Grace LaValle <alavalle3521@sdsu.edu>
2024-04-26 23:54:21 -0700

    edited app.jsx page, worked on about, created team page, linked Footer

---
+++ react-miyabi/src/pages/Team.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Team() {
+  return (
+    <div class="container-sm text-center mt-5 mb-5" style="max-width: 70%;">
+      <strong><u>Team</u></strong>
+    </div>
+
+
+  );
+}
+
+export default Team
