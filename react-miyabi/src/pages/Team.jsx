d6618dd57a0faa03355665d78e9720f3eea9e4b5
Alyssa Grace LaValle <alavalle3521@sdsu.edu>
2024-04-27 00:43:35 -0700

    created a folder "team photos" for headshots, worked on team page

--- react-miyabi/src/pages/Team.jsx
+++ react-miyabi/src/pages/Team.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Alyssa from '../../../docs/team photos/Alyssa.png';
+import Nilesh from '../../../docs/team photos/Nilesh.png';
 
-
-function Team() {
+const Team = () => {
+  const custWidth = {
+    "max-width":"70%",
+}
+  const containerStyle = {
+  "max-width":"70%",
+  "min-width":"60%",
+}
+ try {
   return (
-    <div class="container-sm text-center mt-5 mb-5" style="max-width: 70%;">
-      <strong><u>Team</u></strong>
+    <>
+    <div class="container-sm text-center mt-5 mb-5" style={custWidth}>
+    <h1><strong><u>Meet the Team</u></strong></h1>
+      <img src={Alyssa} />
+      <img src= {Nilesh} />
     </div>
 
-
+  </>
   );
+}catch (error) {
+  console.error('Error rendering About component:', error);
+  return <div>Error occurred. Please check the console for details.</div>;
+}
 }
 
 export default Team
